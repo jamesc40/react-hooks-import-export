e97bf332bc6494c938c32953658b89a8ab0b1741
James Coffman <jamesc4023@gmail.com>
2022-04-25 13:08:00 -0400

    commit tree 0e45b7c57a043c0839390e0100c4ed909a1ccf5d

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,19 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import { 
+  trees as parkTrees, 
+  wildlife as parkWildlife,
+} from './parks/RockyMountain'
 
 function ColoradoStateParks() {
+
+  console.log(parkTrees);
+  parkWildlife();
+  // RMFunctions.elevation();
+
   howManyParks(); // => "42 parks!"
 
   return <h1>Colorado State Parks!</h1>;
 }
+
+export default ColoradoStateParks;
